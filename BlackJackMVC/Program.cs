84024e2a1a431223d335e5d8757758447c726ff7
oldGunCoder <57119330+oldGunCoder@users.noreply.github.com>
2020-06-02 11:27:45 -0400

    standalone app working

---
+++ BlackJackMVC/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+//using System.Windows.Forms;
+
+namespace BlackJackMVC
+{
+    public class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+            //Application.EnableVisualStyles();
+            //Application.Run(new StartForm());
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
